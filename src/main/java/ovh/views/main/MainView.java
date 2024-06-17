2946c4a9f718d07b3ab5366fb7961b06f4701434
MichalWitucki <witucki@outlook.com>
2024-06-17 07:00:43 +0200

    movie card on main page almost finished

--- src/main/java/ovh/views/main/MainView.java
+++ src/main/java/ovh/views/main/MainView.java
@@ -4,13 +4,16 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.theme.lumo.LumoIcon;
 import ovh.views.MainLayout;
 
 @PageTitle("Main")
@@ -22,33 +25,73 @@ public class MainView extends Composite<VerticalLayout> {
     public MainView() {
         VerticalLayout topRow = new VerticalLayout();
         H1 h1 = new H1();
-        Paragraph textSmall = new Paragraph();
+        Paragraph topRowText = new Paragraph();
 
-        VerticalLayout mainContent = new VerticalLayout();
+        HorizontalLayout mainContent = new HorizontalLayout();
+        Image image = new Image();
+        VerticalLayout movieDescriptionColumn = new VerticalLayout();
+        Paragraph polishMovieTitle = new Paragraph();
+        Paragraph englishMovieTitle = new Paragraph();
+        Paragraph yearOfProduction = new Paragraph();
+        Icon rateIcon = VaadinIcon.STAR.create();
+        Paragraph rate = new Paragraph();
 
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
+
         topRow.setWidthFull();
-        getContent().setFlexGrow(1.0, topRow);
+        getContent().setFlexGrow(1.0,topRow);
         topRow.setWidth("100%");
         topRow.getStyle()
                 .set("flex-grow", "1")
                 .set("background-color", "orange");
+        topRow.setAlignSelf(FlexComponent.Alignment.CENTER, topRowText, h1);
+
+        mainContent.setPadding(true);
+        mainContent.setWidthFull();
+        mainContent.getStyle().set("flex-grow", "1");
+
+        movieDescriptionColumn.setHeightFull();
+        mainContent.setFlexGrow(1.0, movieDescriptionColumn);
+        movieDescriptionColumn.setWidth("100%");
+        movieDescriptionColumn.getStyle().set("flex-grow", "1");
+
         h1.setText("MOVIE CLUB");
         h1.setWidth("max-content");
-        textSmall.setText(
+
+        topRowText.setText(
                 "Witaj w najlepszej bazie filmów w sieci. Oceniaj i dyskutuj o " +
                         "najnowszych produkcjach i klasykach kina.");
-        textSmall.setWidth("max-content");
-        topRow.setAlignSelf(FlexComponent.Alignment.CENTER, textSmall, h1);
+        topRowText.setWidth("max-content");
+
+        image.setSrc("images/poster-placeholder.png");
+
+        polishMovieTitle.setText("Tytuł filmu");
+        polishMovieTitle.getStyle()
+                .set("font-size", "var(--lumo-font-size-xl)")
+                .set("font-color", "orange");
+
+        englishMovieTitle.setText("Angielski tytuł filmu");
+
+        yearOfProduction.setText("1988");
+
+        rateIcon.setColor("orange");
+
+        rate.setText("8.7");
+
         getContent().add(topRow);
         getContent().add(mainContent);
+
         topRow.add(h1);
-        topRow.add(textSmall);
+        topRow.add(topRowText);
 
-
-
-        Image image = new Image("images/poster-placeholder.png", "Poster placeholder");
         mainContent.add(image);
+        mainContent.add(movieDescriptionColumn);
+
+        movieDescriptionColumn.add(polishMovieTitle);
+        movieDescriptionColumn.add(englishMovieTitle);
+        movieDescriptionColumn.add(yearOfProduction);
+        movieDescriptionColumn.add(rateIcon);
+        movieDescriptionColumn.add(rate);
     }
 }
